342f48fbc7f6b88ea0971f9165fe15e643e67427
VictorBelle38 <victorbelle38@gmail.com>
2025-09-15 17:21:13 -0400

    Formulario

--- config.js
+++ config.js
@@ -2,3 +2,4 @@
 window.WEBHOOK_URL = 'https://n8n.remotedok.fun/webhook/envio-fotos';
 
 console.log('Configuração carregada:', window.WEBHOOK_URL);
+console.log('Webhook configurado para receber dados em:', window.WEBHOOK_URL);
